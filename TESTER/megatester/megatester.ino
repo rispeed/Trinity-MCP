d23b7b5332a829f2cf6b24c889d637f231cfcaef
Ryan Gralinski <ryan@gralinski.com>
2023-12-19 20:17:29 -0500

    Update

--- TESTER/megatester/megatester.ino
+++ TESTER/megatester/megatester.ino
@@ -1,34 +1,8 @@
 void setup() {
-  // Set all digital pins 2-53 and analog pins 0-14 to INPUT_PULLUP
-  for (int i = 2; i <= 53; i++) {
-    pinMode(i, INPUT_PULLUP);
-  }
-  for (int i = 0; i <= 14; i++) {
-    pinMode(A0 + i, INPUT_PULLUP);
-  }
-
-  // Initialize serial communication
-  Serial.begin(9600);
 }
 
 void loop() {
-  // Check all digital pins 2-53
-  for (int i = 2; i <= 53; i++) {
-    if (digitalRead(i) == LOW) {
-      Serial.print("Digital Pin ");
-      Serial.print(i);
-      Serial.println(" pressed");
-      delay(100); // debounce delay
-    }
+  
   }
 
-  // Check all analog pins 0-14
-  for (int i = 0; i <= 14; i++) {
-    if (digitalRead(A0 + i) == LOW) {
-      Serial.print("Analog Pin ");
-      Serial.print(i);
-      Serial.println(" pressed");
-      delay(100); // debounce delay
-    }
-  }
-}
+  
